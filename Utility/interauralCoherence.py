bb187749b2e47f57c7cec1cb774a0e39a39bed46
Stefan Riedel <35775985+stefanriedel@users.noreply.github.com>
2022-11-07 14:41:59 +0100

    some docstrings for useful utility methods

--- Utility/interauralCoherence.py
+++ Utility/interauralCoherence.py
@@ -1,6 +1,20 @@
 import numpy as np
 
 def compute_IC_Welch(x_L, x_R, gammatone_mag_win, fs, blocksize, hopsize, num_blocks):
+    """ Compute interaural coherence (IC) and monaural ear spectrum using Bartlett/Welch's method.
+    Args:
+        x_L (ndarray): left-ear input signal in time-domain
+        x_R (ndarray): right-ear input signal in time-domain
+        gammatone_mag_win (ndarray): precomputed zero-phase magnitude bandpass windows
+        fs (float): sampling rate in Hz
+        blocksize (int): blocksize in samples for computation of FFT spectrum
+        hopsize (int): hopsize in samples for computation of FFT spectrum
+        num_blocks (int): number of blocks 
+
+    Returns:
+        ndarray: IC [num_bands]
+        ndarray: P_L [num_bands]
+    """
     eps=1e-6
     num_bands = gammatone_mag_win.shape[0]
     tau_limit = 0.001
--- Utility/spatialGranularSynthesis.py
+++ Utility/spatialGranularSynthesis.py
@@ -5,21 +5,37 @@ def hann_window(num_samples):
 
 
 def spatialGranularSynthesis(x, delta_t, grain_length, Q, num_channels, output_length, fs, jitter=0, offset=0, gain=1):
-    # Multichannel granular synthesis function
+    """ Multichannel granular synthesis function.
+    Args:
+        x (ndarray): single-channel audio input buffer
+        delta_t (float): time between grains in sec.
+        grain_length (float): length of grains in sec.
+        Q (float): seed range (maximum grain delay) in sec.
+        num_channels (int): number of output channels
+        output_length (float): length of the synthesis output in sec.
+        fs (float): sample rate in Hz
+        jitter (float): relative jitter amount in percent, default = 0.0
+        offset (int): sample read offset, default = 0
+        gain (float): output gain, default = 1.0
+
+    Returns:
+        ndarray: y [int(output_length*fs), num_channels]
+    """
+
     N = int(output_length*fs)
     y = np.zeros((N,num_channels), dtype=float)
-    max_grain_delay = int(Q*fs)
+    seed_range = int(Q*fs)
 
     grain_samples = int(grain_length*fs)
     window = hann_window(grain_samples)                                                     
     window_gain =  np.sum(window) / grain_samples
     delta_samples = int(delta_t*fs)
 
-    assert(max_grain_delay < (x.size - grain_samples))
+    assert(seed_range < (x.size - grain_samples))
 
     t = 0
     while (t+grain_samples < N):
-        read_idx = np.random.randint(0, max_grain_delay)
+        read_idx = np.random.randint(0, seed_range)
         grain = np.copy(x[(offset+read_idx):(offset+read_idx+grain_samples)])
         grain = grain * window
 
